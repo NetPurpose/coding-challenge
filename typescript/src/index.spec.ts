6475983bf2df09d5605d6b48f45824c47465c8ef
Alex Vamvakas <alex.v@threadsstyling.com>
2022-09-05 20:57:58 +0100

    feat: techtest

--- typescript/src/index.spec.ts
+++ typescript/src/index.spec.ts
@@ -1,5 +1,32 @@
 import { getResults } from ".";
 
+const givenDivision2 = [
+  {
+    name: "Rockets",
+    points: 64,
+  },
+  {
+    name: "Cardinals",
+    points: 77,
+  },
+  {
+    name: "Bruisers",
+    points: 51,
+  },
+  {
+    name: "Toronto Maple Leafs",
+    points: 38,
+  },
+  {
+    name: "Renegades",
+    points: 37,
+  },
+  {
+    name: "Porpoises",
+    points: 52,
+  },
+];
+
 const givenDivision = [
   {
     name: "Rockets",
@@ -43,4 +70,24 @@ Bruisers
 Renegades`;
 
   expect(getResults(givenDivision, 2)).toBe(resultString);
+});
+
+test("returns three teams to promote and two teams to relegate", () => {
+  const resultString = `Promote:
+Cardinals
+Rockets
+Porpoises
+
+Relegate:
+Bruisers
+Toronto Maple Leafs
+Renegades`;
+
+  expect(getResults(givenDivision2, 3)).toBe(resultString);
+});
+
+test("throws error if the teams both in the top n and bottom n for the division", () => {
+  expect(() => getResults(givenDivision, 3)).toThrow(
+    "Team Porpoises is included in both promoted and relegated"
+  );
 });
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,3 +1,30 @@
-export const getResults = (division: unknown, n: unknown) => {
-  // Your code here
+export const getResults = (
+  division: { name: string; points: number }[],
+  n: number
+) => {
+  division = division.sort((a, b) => b.points - a.points); //Sort from highest to lower points
+
+  const relegatedTeams = division.slice(division.length - n, division.length);
+  //Creates the relegated Array with the n Relegated teams sorted from highest to lower points
+
+  let promoteMessage = `Promote:`;
+  let relegateMessage = `Relegate:`;
+
+  for (let i = 0; i < n; i++) {
+    promoteMessage += "\n" + division[i].name;
+    relegateMessage += "\n" + relegatedTeams[i].name;
+  }
+
+  division.map((team) => {
+    if (
+      promoteMessage.includes(team.name) &&
+      relegateMessage.includes(team.name)
+    ) {
+      throw `Team ${team.name} is included in both promoted and relegated`;
+    }
+  });
+
+  const message = promoteMessage + "\n" + "\n" + relegateMessage;
+
+  return message;
 };
